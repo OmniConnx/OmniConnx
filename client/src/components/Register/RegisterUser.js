d149a2be4c6b41ea1f3f114a47d3c9eaaee08a42
Jose <hose153@gmail.com>
2021-11-16 11:49:23 -0600

    Update RegisterUser.js

--- client/src/components/Register/RegisterUser.js
+++ client/src/components/Register/RegisterUser.js
@@ -20,6 +20,26 @@ class RegisterUser extends React.Component {
     authService.register(this.registerUsername.current.value, this.registerPassword.current.value)
     console.log(`Username: ${this.registerUsername.current.value} \nPassword: ${this.registerPassword.current.value}`)
   }
+  // // Example starter JavaScript for disabling form submissions if there are invalid fields
+  // function () {
+  //   'use strict'
+
+  //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  //   var forms = document.querySelectorAll('.needs-validation')
+
+  //   // Loop over them and prevent submission
+  //   Array.prototype.slice.call(forms)
+  //     .forEach(function (form) {
+  //       form.addEventListener('submit', function (event) {
+  //         if (!form.checkValidity()) {
+  //           event.preventDefault()
+  //           event.stopPropagation()
+  //         }
+
+  //         form.classList.add('was-validated')
+  //       }, false)
+  //     })
+  // }
 
   render(){
     return (
@@ -27,9 +47,16 @@ class RegisterUser extends React.Component {
         <h1>Registration</h1>
         <form onSubmit={this.registerUser}>
           <label for="register_username">Username: </label><br />
-          <input type="text" ref={this.registerUsername} ></input><br />
+          <input type="text" id="validationServer01" ref={this.registerUsername} ></input>
+          <div id="validationServerUsernameFeedback" class="invalid-feedback">
+          Please choose a username.
+          </div><br />
+
           <label for="register_password">Password: </label><br />
-          <input type="password" ref={this.registerPassword} ></input><br />
+          <input type="password" id="validationServer01" ref={this.registerPassword} ></input>
+          <div id="validationServerUPasswordFeedback" class="invalid-feedback">
+          Please choose a password.
+          </div><br />
           <input type="button" value="Submit" onClick={this.registerUser}></input>
         </form>
       </div>
